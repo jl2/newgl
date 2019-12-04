3ae6ef691c0c0774c7e59cc0366d4cbb808d59be
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2019-12-04 01:03:18 -0700

    New coloring scheme, more example calls

--- shaders/mandel-fragment.glsl
+++ shaders/mandel-fragment.glsl
@@ -41,9 +41,15 @@ void main (void)
           // tmpval = fract(iter / 8422.0);
           // tmpval2 = fract(iter / 11133.0);
           // color = vec4 (tmpval2, sin(3.1415 * (1.0 - tmpval)), tmpval, 1.0);
-          float red = clamp(imag * real * (iter % 5)/4.0, 0.0, 1.0);
-          float green = clamp(imag * (iter % 3)/2.0, 0.0, 1.0);
-          float blue = clamp(real * real * (iter % 4)/3.0, 0.0, 1.0);
+
+          float red = clamp(imag * real * (iter % 67)/66.0, 0.0, 1.0);
+          float green = clamp(imag * (iter % 47)/46.0, 0.0, 1.0);
+          float blue = clamp(real * real * (iter % 24)/23.0, 0.0, 1.0);
+
+          // float red = clamp(imag * real * (iter % 67)/66.0, 0.0, 1.0);
+          // float green = clamp(imag * (iter % 47)/46.0, 0.0, 1.0);
+          // float blue = clamp(real * real * (iter % 24)/23.0, 0.0, 1.0);
+
           color = vec4(red, green, blue, 1.0);
      }
      Color = color;
